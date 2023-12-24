0779be463217ca668adaf88720ebbaf7db0f558c
Ubuntu <ubuntu@ip-172-31-44-104.ec2.internal>
2023-12-24 18:51:59 +0000

    tested with g4dn.2xlarge

--- serve-with-default-pytorch/update_mar_and_redeploy.sh
+++ serve-with-default-pytorch/update_mar_and_redeploy.sh
@@ -1,6 +1,7 @@
 torchserve --stop
-rm -rf ./model_store/vit_l_16.mar
+rm -rf ./model_store/
 torch-model-archiver --model-name vit_l_16 --version 1.0 --serialized-file vit_l_16.pt --handler custom_handler.py
+mkdir ./model_store
 mv vit_l_16.mar ./model_store
 # torchserve --start --model-store ./model_store --models vit_l_16=vit_l_16.mar --ts-config config.properties
 torchserve --start --ts-config config.properties
